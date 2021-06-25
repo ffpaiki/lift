4749b08ea8eb18cf0445001be8a2e5965a47b94e
ffpaiki <74241058+ffpaiki@users.noreply.github.com>
2021-06-25 22:25:52 +0900

    Final Result

--- js/script.js
+++ js/script.js
@@ -8,17 +8,17 @@ function Elevator(index, currentFloor, html_id, x, y) {
   this.id = html_id;
   this.px = x;
   this.py = y;
-  // this.going = "up";
+  this.distance = 0;
 }
 
-let l1 = new Elevator(1, 20, "l1", -200, 8);
-let l2 = new Elevator(2, 20, "l2", -140, 8);
-let l3 = new Elevator(3, 20, "l3", -80, 8);
+let l1 = new Elevator(1, 20, "l1", -200, 0);
+let l2 = new Elevator(2, 20, "l2", -140, 0);
+let l3 = new Elevator(3, 20, "l3", -80, 0);
+
+const listElevator = [l1, l2, l3];
 
 let divSelectedElevator;
 let distance;
-
-const listElevator = [l1, l2, l3];
 
 const floor = document.querySelectorAll(".floor");
 
@@ -27,9 +27,6 @@ const floor = document.querySelectorAll(".floor");
  */
 function init() {
   for (let i = 20; i > 0; i--) {
-    const divFloorWrapper = document.createElement("div");
-    divFloorWrapper.classList.add("floor-wrapper");
-
     const divFloor = document.createElement("div");
 
     divFloor.classList.add("floor");
@@ -43,36 +40,41 @@ function init() {
     divFloor.addEventListener("click", function (e) {
       callingFloor = i;
       console.log(i);
-      console.log(getNearestElevator(callingFloor));
+      // console.log(getNearestElevator(callingFloor));
 
       selectedElevator = getNearestElevator(callingFloor);
+      // console.log(selectedElevator);
       divSelectedElevator = document.querySelector("#" + selectedElevator.id);
 
       selectedElevator.currentFloor = callingFloor;
 
-      selectedElevator.py = distance * 47;
+      selectedElevator.py +=
+        distance == 0 ? selectedElevator.py : selectedElevator.distance * 41;
 
-      divSelectedElevator.style.transform =
-        "translate(" +
-        selectedElevator.px +
-        ".px, " +
-        selectedElevator.py +
-        "px)";
+      divSelectedElevator.style.top = selectedElevator.py + "px";
+
+      console.log(selectedElevator);
+
+      // divSelectedElevator.style.transform =
+      //   "translate(" +
+      //   selectedElevator.px +
+      //   ".px, " +
+      //   selectedElevator.py +
+      //   "px)";
     });
 
     const divElevators = document.createElement("div");
     divElevators.classList.add("lifts");
 
-    divFloorWrapper.appendChild(divElevators);
-    divFloorWrapper.appendChild(divFloorContainer);
-
-    building.appendChild(divFloorWrapper);
+    building.appendChild(divFloorContainer);
   }
 }
 
 function getNearestElevator(requestedFloor) {
   let minDistance = 100,
-    minIndex;
+    minIndex = 0;
+
+  distance = 0;
 
   listElevator.forEach(function (elevator, index) {
     distance = elevator.currentFloor - requestedFloor;
@@ -82,7 +84,10 @@ function getNearestElevator(requestedFloor) {
     if (Math.abs(distance) < minDistance) {
       minDistance = Math.abs(distance);
       minIndex = index;
+      elevator.distance = distance;
     }
+
+    console.log(minDistance);
   });
 
   return listElevator[minIndex];
