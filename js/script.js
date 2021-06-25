abc59baf83e4c99e4a73b1e849cfbd6d2d8cc816
ffpaiki <74241058+ffpaiki@users.noreply.github.com>
2021-06-25 23:44:17 +0900

    Adding some comments

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const building = document.querySelector(".building");
 
 init();
 
+// Constructor for Elevator object
 function Elevator(index, currentFloor, html_id, x, y) {
   this.currentFloor = currentFloor;
   this.index = index;
@@ -11,6 +12,9 @@ function Elevator(index, currentFloor, html_id, x, y) {
   this.distance = 0;
 }
 
+/**
+ * At the beginning, all three elevator are located in the top floor.
+ */
 let l1 = new Elevator(1, 20, "l1", -200, 0);
 let l2 = new Elevator(2, 20, "l2", -140, 0);
 let l3 = new Elevator(3, 20, "l3", -80, 0);
@@ -23,7 +27,7 @@ let distance;
 const floor = document.querySelectorAll(".floor");
 
 /***
- * Method to initialize all floors and elevators during initialization
+ * Method to initialize all 20 floors and 3 elevators for the building
  */
 function init() {
   for (let i = 20; i > 0; i--) {
@@ -39,11 +43,8 @@ function init() {
 
     divFloor.addEventListener("click", function (e) {
       callingFloor = i;
-      console.log(i);
-      // console.log(getNearestElevator(callingFloor));
 
       selectedElevator = getNearestElevator(callingFloor);
-      // console.log(selectedElevator);
       divSelectedElevator = document.querySelector("#" + selectedElevator.id);
 
       selectedElevator.currentFloor = callingFloor;
@@ -52,42 +53,28 @@ function init() {
         distance == 0 ? selectedElevator.py : selectedElevator.distance * 41;
 
       divSelectedElevator.style.top = selectedElevator.py + "px";
-
-      console.log(selectedElevator);
-
-      // divSelectedElevator.style.transform =
-      //   "translate(" +
-      //   selectedElevator.px +
-      //   ".px, " +
-      //   selectedElevator.py +
-      //   "px)";
     });
-
-    const divElevators = document.createElement("div");
-    divElevators.classList.add("lifts");
 
     building.appendChild(divFloorContainer);
   }
 }
 
+/**
+ * Get the nearest elevator from list of elevators
+ */
+
 function getNearestElevator(requestedFloor) {
   let minDistance = 100,
     minIndex = 0;
 
-  distance = 0;
-
   listElevator.forEach(function (elevator, index) {
     distance = elevator.currentFloor - requestedFloor;
-    console.log(
-      "requested floor: " + requestedFloor + " distance: " + distance
-    );
+
     if (Math.abs(distance) < minDistance) {
       minDistance = Math.abs(distance);
       minIndex = index;
       elevator.distance = distance;
     }
-
-    console.log(minDistance);
   });
 
   return listElevator[minIndex];
